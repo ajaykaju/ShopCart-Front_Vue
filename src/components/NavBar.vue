59e3a777082cc709ff5a8ca328868dd72e443787
ajaykaju <ajukodinhi@gmail.com>
2021-04-09 17:28:37 +0530

    navigation and search bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="top_bar">
+    <div class="top_left">
+      <div class="menu_button mouse_cursor"><img src="../assets/icons/menu-01.png" /></div>
+      <div class="logo mouse_cursor">ShopCart<img src="" /></div>
+    </div>
+    <search-bar class="navbar_search"></search-bar>
+    <div class="top_navigations">
+      <div class="user_welcome">
+        <span>Hello,</span><span class="user_name mouse_cursor"> Sign in</span>
+      </div>
+      <div class="avatar mouse_cursor"><img src="" /></div>
+      <div class="cart mouse_cursor">
+        <span class="cart_items">56</span>
+        <img src="../assets/icons/shopping-cart.png" />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import SearchBar from "./SearchBar.vue";
+export default {
+  components: { SearchBar },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import ../styles/navbar
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="search_bar">
+    <span class="search_icon"><img :src="searchIcon" /></span>
+    <input
+      @focus="searchIconChanger"
+      @blur="searchIconChanger"
+      type="text"
+      class="search_text"
+      placeholder="Search for products"
+    />
+    <span class="filter_icon mouse_cursor"
+      ><img :src="filterIcon" @click="filterIconChanger"
+    /></span>
+  </div>
+</template>
+
+<script>
+import searchIcon from "../assets/icons/search.svg";
+import searchIconBlue from "../assets/icons/search_blue.svg";
+
+import filterIcon from "../assets/icons/sliders.svg";
+import filterIconBlue from "../assets/icons/sliders_blue.svg";
+
+export default {
+  data() {
+    return {
+      isSearchFocused: false,
+      isFilterFocused: false,
+    };
+  },
+  computed: {
+    searchIcon() {
+      if (this.isSearchFocused) return searchIconBlue;
+      else return searchIcon;
+    },
+    filterIcon() {
+      if (this.isFilterFocused) return filterIconBlue;
+      else return filterIcon;
+    },
+  },
+  methods: {
+    searchIconChanger() {
+      this.isSearchFocused = !this.isSearchFocused;
+    },
+    filterIconChanger() {
+      this.isFilterFocused = !this.isFilterFocused;
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.search_bar
+    display: inline-flex
+    place-self: center
+    border-radius: 20px
+    height: 30px
+    background: #ffffff
+    padding: 5px 10px
+    box-shadow: 0 0 4px 0px rgba( 0, 0, 0, 0.1)
+    .search_icon, .filter_icon, .search_text
+        align-items: center
+        align-self: center
+        align-content: center
+        display: flex
+        img
+            width: 20px
+            opacity: 0.8
+    .search_icon
+    .search_text
+        border: none
+        margin: 0 5px 0 5px
+        width: 500px
+        outline: none
+        height: 20px
+        caret-color: #2196f3
+        font-size: 15px
+    .filter_icon
+
+@media (min-width: 1063px)
+    .search_bar
+        .search_text
+            width: 500px
+@media (min-width: 948px) and (max-width: 1062px)
+    .search_bar
+        .search_text
+            width: 400px
+@media (min-width: 0px) and (max-width: 947px)
+    .search_bar
+        .search_text
+            width: 250px
+</style>
