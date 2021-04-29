d440c3b8ea68502a81751b6872badc73ca259c52
ajaykaju <ajukodinhi@gmail.com>
2021-04-29 21:54:56 +0530

    Footer with help links, subscriber, address, and credit

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
     <search-bar class="main_search_bar" ></search-bar>
     <router-view />
   </div>
+  <footer-field></footer-field>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
 import SearchBar from "./components/base/SearchBar.vue";
+import FooterField from './components/FooterField.vue';
 
 export default {
-  components: { NavBar, SearchBar },
+  components: { NavBar, SearchBar, FooterField },
 };
 </script>
 
